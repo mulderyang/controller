2022de68278094271dc54606a120763dfebd9c04
LG Laptop <devin@LG.Laptop>
2018-03-06 14:40:26 +0900

    Initial commit

---
+++ app/src/main/java/com/betheracer/controller/utility/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.betheracer.controller.utility;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.betheracer.controller";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 46;
+  public static final String VERSION_NAME = "1.17.0";
+}
